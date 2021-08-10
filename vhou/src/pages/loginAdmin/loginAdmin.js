a9ffc6b98a3baeff5f3ba86c24d6931e12adf862
Tilteee <carlos.silva-filho@unesp.br>
2021-08-10 00:51:11 -0300

    Faltou umas páginas

---
+++ vhou/src/pages/loginAdmin/loginAdmin.js
@@ -0,0 +1,160 @@
+import React, {Component, useState } from 'react';
+
+// Import de libs de react.
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// Import de Components.
+import LoginComponent from '../../components/login/login';
+import RegisterComponent from '../../components/register/register';
+import ForgotPsdComponent from '../../components/forgotPassword/forgotPassword';
+
+// Import de API.
+// TODO.
+
+// Import de CSS.
+import '../../components/login/login.css';
+import '../../misc/animations.css';
+import '../../misc/misc.css';
+
+class LoginAdmin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      showLogin: true,
+      showForgot: false,
+      loginStatus: false,
+      erro: false,
+    }
+
+    this.addLogin = this.addLogin.bind(this);
+    // this.addRegister = this.addRegister.bind(this);
+    this.addForgotPsd = this.addForgotPsd.bind(this);
+  }
+
+  addLogin() {
+    if (this.state.showLogin === true) {
+      this.state.showCad === false ? this.setState({ showLogin: true,}) : this.setState({  showForgot: false, showCad: false });
+    } else {
+      this.setState({ showLogin: true,  showForgot: false, showCad: false });
+    }
+  }
+
+//   addRegister() {
+//     if (this.state.showCad === true) {
+//       this.state.showLogin === false ? this.setState({ showCad: true }) : this.setState({ showLogin: false, showForgot: false });
+//     } else {
+//       this.setState({ showCad: true, showLogin: false, showForgot: false});
+//     }
+//   }
+
+  addForgotPsd() {
+    if (this.state.showForgot === true) {
+      (this.state.showLogin === false || this.state.showCad === false) ? this.setState({ showForgot: true }) : this.setState({ showCad: false, showLogin: false });
+    } else {
+      this.setState({ showCad: false, showLogin: false, showForgot: true});
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div id="main">
+          <div className="blur-section">a</div>
+          <h1 className="text-center login-title noselect">Visualizador de horas online universitário Cacic</h1>
+          <div id="contain" className="container">
+            <div className="login-register-wrapper animadoCimaParaBaixo">
+              <div className="form-group">
+                <LoginComponent showLogin={this.state.showLogin}></LoginComponent>
+                <ForgotPsdComponent showForgot={this.state.showForgot}></ForgotPsdComponent>
+              </div>
+              <div id="forgot-panel">
+                <a className="forgot-password text-center noselect" onClick={this.addForgotPsd} href="#forgot">Já possui cadastro e esqueceu a senha ?</a>
+                <div id="text-forgot-panel">
+                  <p>
+                    {this.state.loginStatus === true
+                      ? `Logado com sucesso`
+                      : this.state.loginStatus === false
+                        ? this.state.erro
+                        : ``}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+
+// const Login = () =>{
+//   const [show, setShow] = useState(false);
+//   const [showLogin, setShowLogin] = useState(false);
+//   const [showCad, setShowCad] = useState(false);
+//   const [showForgot, setShowForgot] = useState(false);
+//   const [loginStatus, setLoginStatus] = useState(false);
+//   const {erro} = false;
+  
+//   function addLogin() {
+//     if (showLogin === true) {
+//       showCad === false ? setShowLogin(true) : setShowForgot(false) && setShowCad(false);
+//     } else {
+//       setShowLogin(true) && setShowForgot(false) && setShowCad(false);
+//     }
+//   }
+
+//   function addRegister() {
+//     if (showCad === true) {
+//       showLogin === false ? setShowCad(true) : setShowLogin(false) && setShowForgot(false);
+//     } else {
+//       setShowCad(true) && setShowLogin(false) && setShowForgot(false);
+//     }
+//   }
+
+//   function addForgotPsd() {
+//     if (showForgot === true) {
+//       (showLogin === false || showCad === false) ? setShowForgot(true) : setShowCad(false) &&  setShowLogin(false);
+//     } else {
+//       setShowForgot(true) && setShowCad(false) &&  setShowLogin(false);
+//     }
+//   }
+
+//     return (
+//       <>
+//         <div id="main">
+//           <div className="blur-section">a</div>
+//           <h1 className="text-center login-title noselect">Visualizador de horas online universitário Cacic</h1>
+//           <div id="contain" className="container">
+//             <div className="login-register-wrapper animadoCimaParaBaixo">
+//               <div className="nav-buttons">
+//                 <button type="button" className="btn" onClick={addLogin()} id="toggleBtn">Login</button>
+//                 <button type="button" className="btn" onClick={addRegister()} id="toggleBtn">Cadastro</button>
+//               </div>
+//               <div className="form-group">
+//                 <LoginComponent showLogin={showLogin}></LoginComponent>
+//                 <RegisterComponent showCad={showCad}></RegisterComponent>
+//                 <ForgotPsdComponent showForgot={showForgot}></ForgotPsdComponent>
+//               </div>
+//               <div id="forgot-panel">
+//                 <a className="forgot-password text-center noselect" onClick={addForgotPsd()} href="#forgot">Já possui cadastro e esqueceu a senha ?</a>
+//                 <div id="text-forgot-panel">
+//                   <p>
+//                     {loginStatus === true
+//                       ? `Logado com sucesso`
+//                       : loginStatus === false
+//                         ? erro
+//                         : ``}
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </>
+//     );
+// }
+
+export default LoginAdmin;
---
+++ vhou/src/pages/menuAdmin/menuAdmin.js
@@ -0,0 +1,126 @@
+import React from 'react';
+
+// Import de libs de react
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// Import de Componentes
+import Navbar from '../../components/navbar/navbar';
+import BarChart from '../../components/Bars/barChart';
+import { DocumentsSent, DocumentsApproved, DocumentsPending } from '../../components/documentsArea/documentsArea';
+
+// Import de API
+// TODO.
+
+// Import de CSS.
+import '../menu/menu.css';
+import '../../components/sendDocument/sendDocument.css';
+import '../../misc/animations.css';
+import '../../misc/misc.css';
+
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showActivitiesSent: true,
+      showActivitiesApproved: false,
+      showPending: false,
+      allowSearch: false,
+    }
+  }
+
+  allowSearch() {
+    var elementos = document.getElementsByName('radioPesquisar');
+    elementos.forEach(element => {
+      if(element.checked == true){
+        var x = document.getElementById("inputPesquisar");
+        x.disabled = false;
+        console.log(x);
+        return;
+      }
+    });
+  }
+  showActSent() {
+    this.state.showActivitiesSent === false ?
+      this.setState({ showActivitiesSent: true, showActivitiesApproved: false, showPending: false }) :
+      this.setState({ showActivitiesSent: false, showActivitiesApproved: false, showPending: false });
+  }
+
+  showActAproved() {
+    this.state.showActivitiesApproved === false ?
+      this.setState({ showActivitiesSent: false, showActivitiesApproved: true, showPending: false }) :
+      this.setState({ showActivitiesSent: false, showActivitiesApproved: false, showPending: false });
+  }
+
+  showPending() {
+    this.state.showPending === false ?
+      this.setState({ showActivitiesSent: false, showActivitiesApproved: false, showPending: true }) :
+      this.setState({ showActivitiesSent: false, showActivitiesApproved: false, showPending: false });
+  }
+
+  render() {
+    return (
+      <>
+        <div id="main-menu">
+          <Navbar />
+          <div className="container info">
+            <div className="row">
+              <div className="col-md-4 list-menu-options">
+                <div className="list-menu list-group" id="list-tab" role="tablist">
+                  <div className="row" style={{margin: '0 1px'}}>
+                    <div className='col-lg-6 col-md-8 col-sm-8'>
+                      <input type="radio" name="radioPesquisar" id="pesquisarNome" onChange={() => this.allowSearch()}/>&nbsp;&nbsp;
+                      <label >
+                        Nome
+                      </label>&nbsp;&nbsp;
+                      <input type="radio" name="radioPesquisar" id="pesquisarAno" onChange={() => this.allowSearch()}/>&nbsp;&nbsp;
+                      <label >
+                        Ano
+                      </label>
+                    </div>
+                  </div>
+                  <input className="form-control" name="inputPesquisar" id="inputPesquisar" type="text" placeholder="Pesquisar.." style={{margin: '0 7px'}} disabled/>
+                  <button className="list-menu-item list-group-item list-group-item-action noselect" id="act-approved" onClick={() => this.showActSent()} href="#list-act-approved" role="tab" aria-controls="atividades extras enviadas aprovadas">Atividades Extras Enviadas</button>
+                  <button className="list-menu-item list-group-item list-group-item-action noselect" id="act-pending" onClick={() => this.showActAproved()} href="#list-act-pending" role="tab" aria-controls="atividades extras aprovadas">Atividades Extras Aprovadas</button>
+                  <button className="list-menu-item list-group-item list-group-item-action noselect" id="cord-contact" onClick={() => this.showPending()} href="#list-cord-pending" role="tab" aria-controls="atividades extras pendentes">Atividades Extras Pendentes</button>
+                </div>
+              </div>
+              <div className="col-md-8 hours">
+                <div id="imprime">
+                  <BarChart BarTitles={["Monitoria A", "Secomp", "Monitoria B", "Projeto", "Monitoria C", "Bateria", "Tempo no Cacic", "Ajuda assitencial ao governo",]} ActivityName={['A']} data={[6, 5, 8, 1, 6, 5, 4,]}></BarChart>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col list-menu-content">               
+                <div className={`${(this.state.showActivitiesSent === false) ? "nodisplay" : "showdisplay"}`}>
+                  <DocumentsSent isCord={true}></DocumentsSent>
+                </div>
+                <div className={`${(this.state.showActivitiesApproved === false) ? "nodisplay" : "showdisplay"}`}>
+                  <DocumentsApproved></DocumentsApproved>
+                </div>
+                <div className={`${(this.state.showPending === false) ? "nodisplay" : "showdisplay"}`}>
+                  <DocumentsPending></DocumentsPending>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container" id="about">
+            <div className="row">
+              <div className="col">
+                <div><h2 className="text-center">Sobre</h2></div>
+                <div>
+                  <p className="text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit nisl vitae neque sodales placerat. Mauris leo lacus, ultricies sed egestas eget, gravida a magna. Quisque efficitur lectus a condimentum egestas. Curabitur erat sapien, tempus dictum orci ut, vulputate eleifend ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam sed varius mi, vitae suscipit orci. Nulla ante eros, blandit in ultricies sed, tincidunt et ligula. Cras blandit accumsan sem, ac eleifend purus ornare quis. Vestibulum eget sem eu leo vulputate placerat non sed justo. Maecenas lacinia dignissim mauris nec vulputate.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+
+} export default Menu;
