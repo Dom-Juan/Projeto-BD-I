790c1a0f6561e6463803e654683fa360ba7def92
Juan Cardoso da Silva <Juancardosodasilva@gmail.com>
2022-01-11 19:00:34 -0300

    [Tela Documentos] - Adicionado Download de arquivos e avaliação.

--- vhou/src/components/documentsArea/documentsArea.js
+++ vhou/src/components/documentsArea/documentsArea.js
@@ -1,18 +1,172 @@
 import React, { useState } from 'react';
 
 // Import de libs de react
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import FileSaver from 'file-saver';
+import fileDownload from 'js-file-download';
 
 // Import de API
-//import api from '../../pages/api';
+import api from '../../pages/api';
+import { getUser } from '../../pages/auth';
 
 // Import de CSS.
 import './documents.css';
 import '../../misc/animations.css';
 import '../../misc/misc.css';
 
+const handleDownload = (filename) => {
+  const URL = "http://localhost:3333/atividade/get-file/";
+  console.log(URL);
+  api.get(URL + filename, {
+    responseType: 'blob',
+  }).then((response) => {
+    fileDownload(response.data, filename);
+  });
+}
+
 const DocumentsSent = (props) => {
+  const [s, setAval] = useState();
+  const [informacoes, setInformacoes] = useState({});
+
+  const setInformacoesForm = (event) => {
+    event.persist();
+    setInformacoes(informacoes => ({
+      ...informacoes,
+      [event.target.name]: event.target.value,
+    }));
+    console.log(informacoes);
+  };
+
+  const setAvaliacao = (event) => {
+    let s1 = event.target.value;
+    setAval(s1);
+    console.log(s);
+  }
+
+  const HandleUpdate = (e) => {
+    e.preventDefault();
+    let status_atividade = informacoes.status_atividade;
+    let id_coord_usuario = getUser();
+    let nome_atividade = informacoes.nome_atividade;
+    console.log(status_atividade, id_coord_usuario, nome_atividade);
+
+    if (nome_atividade === undefined || id_coord_usuario === undefined || status_atividade === undefined) {
+      alert("Preencha todos os valores antes de aprovar!!");
+      return;
+    }
+
+    try {
+      api.put(`/atividade/avaliar/`, { nome_atividade, id_coord_usuario, status_atividade }).then(response => {
+        console.log(response);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div id="main-document">
+      <div className="container">
+        {
+          props.vetor.map((element, index) => (
+            <div>
+              {/* Mostrar apenas atividades pendentes abaixo */}
+              {(element.status_atividade === "pendente") ? (
+                <div className="card-doc card text-center" id="card" key={index}>
+                  <h3 className="card-header document-bkg noselect" style={{ background: 'var(--primary)' }}>Atividade Extracurricular</h3>
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col text-center noselect">
+                        <p name="id_atividade" value={element.id_atividade}>{element.nome_atividade}</p>
+                      </div>
+                      <hr />
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <h3><p className="noselect">Informações do Aluno:</p></h3>
+                        <div className="table-responsive">
+                          <table className="table">
+                            <thead>
+                              <tr>
+                                <th scope="col noselect">RA</th>
+                                <th scope="col noselect">HORAS COMPLEMENTARES</th>
+                                <th scope="col noselect">TIPO</th>
+                                <th scope="col noselect">DATA DE INÍCIO</th>
+                                <th scope="col noselect">DATA DE FIM</th>
+                                <th scope="col noselect">ARQUIVO</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <th scope="row">{element.ra_aluno_atividade}</th>
+                                <td>{element.horas_atividade}</td>
+                                <td>{element.tipo_atividade}</td>
+                                <td>{element.data_ini_atividade}</td>
+                                <td>{element.data_fim_atividade}</td>
+                                <td>{element.url_atividade}</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                    {(props.isCord === true) ?
+                      <div className="buttonsEdit">
+                        <div className="container">
+                          <div className="row">
+                            <form onSubmit={HandleUpdate}>
+                              <div className="col">
+                                <label>
+                                  Digite o nome da atividade para confirmar:
+                                </label>
+                                <input className="docInput" name="nome_atividade" onChange={setInformacoesForm} />
+                              </div>
+                              <div className="col">
+                                <li className="form-check">
+                                  <input className="form-check-input" type="radio" name="status_atividade" id="bRadios1" value="aprovado" onChange={setInformacoesForm} />
+                                  <label className="form-check-label" htmlFor="bRadios1">
+                                    Aprovado
+                                  </label>
+                                </li>
+                                <li className="form-check">
+                                  <input className="form-check-input" type="radio" name="status_atividade" id="bRadios2" value="reprovado" onChange={setInformacoesForm} />
+                                  <label className="form-check-label" htmlFor="bRadios2">
+                                    Reprovado
+                                  </label>
+                                </li>
+                              </div>
+                              <div className="col btnDoc">
+                                <button button type="submit" className="btn btnSubmitApprove" id="toggleBtn">Avaliar</button>
+                              </div>
+                            </form>
+                          </div>
+                          <div className="row">
+                            <div className="col btnDoc">
+                              <a type="button" className="btn btnSubmit2" id="toggleBtn" onClick={() => handleDownload(String(element.url_atividade))}>Download</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      : ''
+                    }
+
+                  </div>
+                  <div className='card-footer text-muted bg-warning noselect'>
+                    <p className="text-center" style={{ color: 'white' }}>{element.status_atividade}</p>
+                  </div>
+                </div>
+              ) : ""}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+const DocumentsApproved = (props) => {
   const isCord = useState(props.isCord === true ? true : false);
 
   return (
@@ -32,112 +186,38 @@ const DocumentsSent = (props) => {
                 <div className="row">
                   <div className="col">
                     <h3><p className="noselect">Informações do Aluno:</p></h3>
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col noselect">RA</th>
-                          <th scope="col noselect">HORAS COMPLEMENTARES</th>
-                          <th scope="col noselect">TIPO</th>
-                          <th scope="col noselect">DATA DE INÍCIO</th>
-                          <th scope="col noselect">DATA DE FIM</th>
-                          <th scope="col noselect">ARQUIVO</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th scope="row">{element.ra_aluno_atividade}</th>
-                          <td>{element.horas_atividade}</td>
-                          <td>{element.tipo_atividade}</td>
-                          <td>{element.data_ini_atividade}</td>
-                          <td>{element.data_fim_atividade}</td>
-                          <td>{element.url_atividade}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-                {(isCord[0]) ?
-                  <div className="buttonsEdit">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col btnDoc">
-                          <button type="button" className="btn btnSubmitApprove" id="toggleBtn">Aprovar</button>
-                        </div>
-                        <div className="col btnDoc">
-                          <button type="button" className="btn btnSubmitClose" id="toggleBtn">Reprovar</button>
-                        </div>
-                        <div className="col btnDoc">
-                          <button type="button" className="btn btnSubmit2" id="toggleBtn">Download</button>
-                        </div>
-                      </div>
+                    <div className="table-responsive">
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th scope="col noselect">RA</th>
+                            <th scope="col noselect">HORAS COMPLEMENTARES</th>
+                            <th scope="col noselect">TIPO</th>
+                            <th scope="col noselect">DATA DE INÍCIO</th>
+                            <th scope="col noselect">DATA DE FIM</th>
+                            <th scope="col noselect">ARQUIVO</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">{element.ra_aluno_atividade}</th>
+                            <td>{element.horas_atividade}</td>
+                            <td>{element.tipo_atividade}</td>
+                            <td>{element.data_ini_atividade}</td>
+                            <td>{element.data_fim_atividade}</td>
+                            <td>{element.url_atividade}</td>
+                          </tr>
+                        </tbody>
+                      </table>
                     </div>
                   </div>
-                  : ''
-                }
-
-              </div>
-              <div className='card-footer text-muted bg-warning noselect'>
-                <p className="text-center" style={{ color: 'white' }}>{element.status_atividade}</p>
-              </div>
-            </div>
-
-          ))
-        }
-      </div>
-    </div>
-  );
-}
-
-const DocumentsApproved = (props) => {
-  const isCord = useState(props.isCord === true ? true : false);
-
-  return (
-    <div id="main-document">
-      <div className="container">
-      {
-          props.vetor.map((element, index) => (
-            <div className="card-doc card text-center" id="card" key={index}>
-              <h3 className="card-header document-bkg noselect" style={{ background: 'var(--primary)' }}>Atividade Extracurricular</h3>
-              <div className="card-body">
-                <div className="row">
-                  <div className="col text-center noselect">
-                    {element.nome_atividade}
-                  </div>
-                  <hr />
                 </div>
-                <div className="row">
-                  <div className="col">
-                    <h3><p className="noselect">Informações do Aluno:</p></h3>
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col noselect">RA</th>
-                          <th scope="col noselect">HORAS COMPLEMENTARES</th>
-                          <th scope="col noselect">TIPO</th>
-                          <th scope="col noselect">DATA DE INÍCIO</th>
-                          <th scope="col noselect">DATA DE FIM</th>
-                          <th scope="col noselect">ARQUIVO</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th scope="row">{element.ra_aluno_atividade}</th>
-                          <td>{element.horas_atividade}</td>
-                          <td>{element.tipo_atividade}</td>
-                          <td>{element.data_ini_atividade}</td>
-                          <td>{element.data_fim_atividade}</td>
-                          <td>{element.url_atividade}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-                {(isCord[0]) ?
+                {(props.isCord) ?
                   <div className="buttonsEdit">
                     <div className="container">
                       <div className="row">
                         <div className="col btnDoc">
-                          <button type="button" className="btn btnSubmit2" id="toggleBtn">Download Arquivo</button>
+                          <button type="button" className="btn btnSubmit2" id="toggleBtn" onClick={() => handleDownload(String(element.url_atividade))}>Download</button>
                         </div>
                       </div>
                     </div>
@@ -164,7 +244,7 @@ const DocumentsPending = (props) => {
   return (
     <div id="main-document">
       <div className="container">
-      {
+        {
           props.vetor.map((element, index) => (
             <div className="card-doc card text-center" id="card" key={index}>
               <h3 className="card-header document-bkg noselect" style={{ background: 'var(--primary)' }}>Atividade Extracurricular</h3>
@@ -178,31 +258,33 @@ const DocumentsPending = (props) => {
                 <div className="row">
                   <div className="col">
                     <h3><p className="noselect">Informações do Aluno:</p></h3>
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col noselect">RA</th>
-                          <th scope="col noselect">HORAS COMPLEMENTARES</th>
-                          <th scope="col noselect">TIPO</th>
-                          <th scope="col noselect">DATA DE INÍCIO</th>
-                          <th scope="col noselect">DATA DE FIM</th>
-                          <th scope="col noselect">ARQUIVO</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th scope="row">{element.ra_aluno_atividade}</th>
-                          <td>{element.horas_atividade}</td>
-                          <td>{element.tipo_atividade}</td>
-                          <td>{element.data_ini_atividade}</td>
-                          <td>{element.data_fim_atividade}</td>
-                          <td>{element.url_atividade}</td>
-                        </tr>
-                      </tbody>
-                    </table>
+                    <div className="table-responsive">
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th scope="col noselect">RA</th>
+                            <th scope="col noselect">HORAS COMPLEMENTARES</th>
+                            <th scope="col noselect">TIPO</th>
+                            <th scope="col noselect">DATA DE INÍCIO</th>
+                            <th scope="col noselect">DATA DE FIM</th>
+                            <th scope="col noselect">ARQUIVO</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">{element.ra_aluno_atividade}</th>
+                            <td>{element.horas_atividade}</td>
+                            <td>{element.tipo_atividade}</td>
+                            <td>{element.data_ini_atividade}</td>
+                            <td>{element.data_fim_atividade}</td>
+                            <td>{element.url_atividade}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
-                {(isCord[0]) ?
+                {(props.isCord) ?
                   <div className="buttonsEdit">
                     <div className="container">
                       <div className="row">
@@ -213,7 +295,7 @@ const DocumentsPending = (props) => {
                           <button type="button" className="btn btnSubmitClose" id="toggleBtn">Reprovar</button>
                         </div>
                         <div className="col btnDoc">
-                          <button type="button" className="btn btnSubmit2" id="toggleBtn">Download Arquivo</button>
+                          <button type="button" className="btn btnSubmit2" id="toggleBtn" onClick={() => handleDownload(String(element.url_atividade))}>Download</button>
                         </div>
                       </div>
                     </div>
--- vhou/src/pages/menuAdmin/menuAdmin.js
+++ vhou/src/pages/menuAdmin/menuAdmin.js
@@ -10,7 +10,8 @@ import Navbar from '../../components/navbar/navbar';
 import { DocumentsSent, DocumentsApproved, DocumentsPending } from '../../components/documentsArea/documentsArea';
 
 // Import de API
-// TODO.
+import api from '../api';
+import { getUser } from '../auth';
 
 // Import de CSS.
 import '../menu/menu.css';
@@ -19,6 +20,199 @@ import '../../components/documentsArea/documents.css';
 import '../../misc/animations.css';
 import '../../misc/misc.css';
 
+const Menu = () => {
+  // Variáveis para as ativiadades.
+  const [showActivitiesSent, setActSent] = useState();
+  const [showActivitiesApproved, setActApproved] = useState();
+  const [showPending, setActPending] = useState();
+  const [AllowSearch, setAllowSearch] = useState();
+
+  // Texto monstrando a resposta para o usuário.
+  const [text, setText] = useState("");
+
+  // Variáveis para o fetch do banco de dados.
+  const [cursosPegos, setCursosPegos] = useState([]);
+  const [horasPegas, setHorasPegas] = useState([]);
+  const [documents, setDocument] = useState([]);
+  const [atividades, setAtividades] = useState([]);
+  const [horas, setHoras] = useState([]);
+
+  useEffect(() => {
+    getCursos();
+    getHorasComplementares();
+    getDocumentsAdmin();
+  }, []);
+
+  async function getCursos() {
+    await api.get(`/curso/todos`).then(response => {
+      setCursosPegos(response.data.cursos);
+    });
+  }
+
+  async function getHorasComplementares() {
+    await api.get(`/horas/todas`).then(response => {
+      setHorasPegas(response.data.horas_complementares);
+    });
+  }
+
+  async function getDocumentsAdmin() {
+    api.get(`/atividade/todas`).then(response => {
+      let arrayDocumentosUsuario = [];
+      let arrayGraficoAtividade = [];
+      let arrayGraficoHora = [];
+      response.data.atividades.forEach(element => {
+        arrayDocumentosUsuario.push(element);
+        arrayGraficoAtividade.push(element.nome_atividade);
+        arrayGraficoHora.push(element.horas_atividade);
+      });
+      setDocument(arrayDocumentosUsuario);
+    });
+  }
+
+  function setShowActSent() {
+    if (showActivitiesSent === false) {
+      setActSent(true);
+      setActApproved(false);
+      setActPending(false);
+    } else {
+      setActSent(false);
+      setActApproved(false);
+      setActPending(false);
+    }
+  }
+
+  function setShowActAproved() {
+    if (showActivitiesApproved === false) {
+      setActSent(false);
+      setActApproved(true);
+      setActPending(false);
+    } else {
+      setActSent(false);
+      setActApproved(false);
+      setActPending(false);
+    }
+  }
+
+  function setShowPending() {
+    if (showPending === false) {
+      setActSent(false);
+      setActApproved(false);
+      setActPending(true);
+    } else {
+      setActSent(false);
+      setActApproved(false);
+      setActPending(false);
+    }
+  }
+
+  function setAllowSearchInput() {
+    setAllowSearch(true);
+  }
+
+  function allowSearch() {
+    var elementos = document.getElementsByName('radioPesquisar');
+    elementos.forEach(element => {
+      if(element.checked === true){
+        var x = document.getElementById("inputPesquisar");
+        x.disabled = false;
+        console.log(x);
+      }
+    });
+  }
+
+  const filter = (e) => {
+    const keyword = e.target.value;
+    let result = []
+    if(keyword !== '') {
+      documents.forEach((index, element) => {
+        if(keyword === element.ra_aluno_atividade) {
+          result.append(element);
+        }
+        if(keyword === element.nome_atividade) {
+          result.append(element);
+        }
+      });
+      setDocument(result);
+    } else {
+      getDocumentsAdmin();
+      result = documents;
+    }
+  }
+
+    return (
+      <>
+        <div id="main-menu">
+        <Navbar pathname={"/admin/"} isAdmin={true}/>
+          <div className="container info">
+            <div className="row">
+              <div className="col list-menu-options">
+                <div className="list-menu list-group" id="list-tab" role="tablist">
+                  <div className="row" style={{margin: '0 1px'}}>
+                    <div className='col-lg-6 col-md-8 col-sm-8'>
+                      <input type="radio" name="radioPesquisar" id="pesquisarNome" onChange={() => setAllowSearchInput()}/>&nbsp;&nbsp;
+                      <label >
+                        Nome
+                      </label>&nbsp;&nbsp;
+                      <input type="radio" name="radioPesquisar" id="pesquisarAno" onChange={() => setAllowSearchInput()}/>&nbsp;&nbsp;
+                      <label >
+                        Ano
+                      </label>
+                    </div>
+                  </div>
+                  <input className="form-control" name="inputPesquisar" id="inputPesquisar" type="text" placeholder="Pesquisar.." style={{margin: '0 7px'}} disabled/>
+                  <div className="opt-group">
+                    <button className="list-menu-item list-group-item list-group-item-action noselect" id="act-approved" onClick={() => setShowActSent()} href="#list-act-approved" role="tab" aria-controls="atividades extras enviadas aprovadas">Atividades Extras Recebidas</button>
+                    <button className="list-menu-item list-group-item list-group-item-action noselect" id="act-pending" onClick={() => setShowActAproved()} href="#list-act-pending" role="tab" aria-controls="atividades extras aprovadas">Atividades Extras Avaliadas</button>
+                    <button className="list-menu-item list-group-item list-group-item-action noselect" id="cord-contact" onClick={() => setShowPending()} href="#list-cord-pending" role="tab" aria-controls="atividades extras pendentes">Atividades Extras Pendentes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col list-menu-content">              
+                <div className={`${(showActivitiesSent === false) ? "nodisplay" : "showdisplay"}`}>
+                  <DocumentsSent vetor={documents} isCord={true}></DocumentsSent>
+                </div>
+                <div className={`${(showActivitiesApproved === false) ? "nodisplay" : "showdisplay"}`}>
+                  <DocumentsApproved vetor={documents} isCord={true}></DocumentsApproved>
+                </div>
+                <div className={`${(showPending === false) ? "nodisplay" : "showdisplay"}`}>
+                  <DocumentsPending vetor={documents} isCord={true}></DocumentsPending>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container" id="about">
+            <div className="row">
+              <div className="col">
+                <div><h2 className="text-center">Sobre</h2></div>
+                <div>
+                  <p className="text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit nisl vitae neque sodales placerat. Mauris leo lacus, ultricies sed egestas eget, gravida a magna. Quisque efficitur lectus a condimentum egestas. Curabitur erat sapien, tempus dictum orci ut, vulputate eleifend ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam sed varius mi, vitae suscipit orci. Nulla ante eros, blandit in ultricies sed, tincidunt et ligula. Cras blandit accumsan sem, ac eleifend purus ornare quis. Vestibulum eget sem eu leo vulputate placerat non sed justo. Maecenas lacinia dignissim mauris nec vulputate.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  /*
+  <div className='col-lg-6 col-md-8 col-sm-8'>
+                      <input type="radio" name="radioPesquisar" id="pesquisarNome" onChange={() => setAllowSearchInput()}/>&nbsp;&nbsp;
+                      <label >
+                        Nome
+                      </label>&nbsp;&nbsp;
+                      <input type="radio" name="radioPesquisar" id="pesquisarAno" onChange={() => setAllowSearchInput()}/>&nbsp;&nbsp;
+                      <label >
+                        Ano
+                      </label>
+                    </div>
+  */
+
+/*
 class Menu extends React.Component {
   constructor(props) {
     super(props);
@@ -37,7 +231,6 @@ class Menu extends React.Component {
         var x = document.getElementById("inputPesquisar");
         x.disabled = false;
         console.log(x);
-        return;
       }
     });
   }
@@ -119,6 +312,5 @@ class Menu extends React.Component {
       </>
     );
   }
-
-
-} export default Menu;
+} */
+export default Menu;
